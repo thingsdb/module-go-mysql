003d6c658646e1ba1c81c0ac498c6c878f87f4b0
AnjaBruls <anja@transceptor.technology>
2021-06-09 18:09:10 +0200

    first commit mysql module

---
+++ main.go
@@ -0,0 +1,211 @@
+// Demo is a ThingsDB module which may be used as a template to build modules.
+//
+// This module simply extract a given `message` property from a request and
+// returns this message.
+//
+// For example:
+//
+//     // Create the module (@thingsdb scope)
+//     new_module('DEMO', 'demo', nil, nil);
+//
+//     // When the module is loaded, use the module in a future
+//     future({
+//       module: 'DEMO',
+//       message: 'Hi ThingsDB module!',
+//     }).then(|msg| {
+//	      `Got the message back: {msg}`
+//     });
+//
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	timod "github.com/thingsdb/go-timod"
+	"github.com/vmihailenco/msgpack"
+)
+
+var db *sql.DB = nil
+var mux sync.Mutex
+
+
+type confMySQL struct {
+	Dsn string         `msgpack:"dsn"`
+	ConnMaxLifetime int64         `msgpack:"conn_max_lifetime"`
+	MaxOpenConn int         `msgpack:"max_open_conn"`
+	MaxIdleConn  int `msgpack:"max_idle_conn"`
+}
+
+type reqMySQL struct {
+	Query   string `msgpack:"query"`
+	Params  []interface{} `msgpack:"params"`
+	Fetch string `msgpack:"fetch"` // []string?
+	Transaction bool `msgpack:"transaction"`
+	Timeout int `msgpack:"timeout"`
+}
+
+func handleConf(config *confMySQL) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	if db != nil {
+		db.Close()
+	}
+
+	var err error
+	db, err = sql.Open("mysql", config.Dsn)
+	if err != nil {
+		// TODO
+		timod.WriteConfErr()
+		panic(err.Error())  // Just for example purpose. You should use proper error handling instead of panic
+	}
+
+	db.SetConnMaxLifetime(time.Minute * time.Duration(config.ConnMaxLifetime))
+	db.SetMaxOpenConns(config.MaxOpenConn)
+	db.SetMaxIdleConns(config.MaxIdleConn)
+
+	timod.WriteConfOk()
+}
+
+func handleQuery(pkg *timod.Pkg, req *reqMySQL) {
+	if req.Timeout == 0 {
+		req.Timeout = 10
+	}
+
+	ctx, cancelfunc := context.WithTimeout(context.Background(), time.Duration(req.Timeout) * time.Second)
+    defer cancelfunc()
+
+	ret, err := execQuery(db, ctx, req)
+	if err != nil {
+		timod.WriteEx(
+			pkg.Pid,
+			timod.ExOperation,
+			err.Error())
+		return
+	}
+
+	timod.WriteResponse(pkg.Pid, ret)
+}
+
+func handleTransaction(pkg *timod.Pkg, req *reqMySQL) {
+	if req.Timeout == 0 {
+		req.Timeout = 10
+	}
+
+	ctx, cancelfunc := context.WithTimeout(context.Background(), time.Duration(req.Timeout) * time.Second)
+    defer cancelfunc()
+	tx, err := db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable}) // Tx options?
+	if err != nil {
+		timod.WriteEx(
+			pkg.Pid,
+			timod.ExOperation,
+			fmt.Sprintf("Failed to start transaction: %s", err))
+		return
+	}
+	defer tx.Rollback() // The rollback will be ignored if the tx has been committed later in the function.
+
+	ret, err := execQuery(tx, ctx, req)
+	if err != nil {
+		timod.WriteEx(
+			pkg.Pid,
+			timod.ExOperation,
+			fmt.Sprintf("Failed to execute transaction: %s", err))
+		return
+	}
+
+	if err := tx.Commit(); err != nil {
+		timod.WriteEx(
+			pkg.Pid,
+			timod.ExOperation,
+			fmt.Sprintf("Failed to commit transaction: %s", err))
+		return
+	}
+
+	timod.WriteResponse(pkg.Pid, ret)
+}
+
+func onModuleReq(pkg *timod.Pkg) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	// db.Stats()
+
+	if db == nil || db.Ping() != nil {
+		timod.WriteEx(
+			pkg.Pid,
+			timod.ExOperation,
+			"Error: MySQL is not connected; please check the module configuration")
+		return
+	}
+
+	var req reqMySQL
+	err := msgpack.Unmarshal(pkg.Data, &req)
+	if err != nil {
+		timod.WriteEx(
+			pkg.Pid,
+			timod.ExBadData,
+			"Error: Failed to unpack MySQL request")
+		return
+	}
+
+	if req.Query == "" {
+		timod.WriteEx(
+			pkg.Pid,
+			timod.ExBadData,
+			"Error: MySQL requires `query`")
+		return
+	}
+
+	if req.Transaction {
+		handleTransaction(pkg, &req)
+	} else {
+		handleQuery(pkg, &req)
+	}
+	// Todo STATS
+}
+
+func handler(buf *timod.Buffer, quit chan bool) {
+	for {
+		select {
+		case pkg := <-buf.PkgCh:
+			switch timod.Proto(pkg.Tp) {
+			case timod.ProtoModuleConf:
+				var conf confMySQL
+				err := msgpack.Unmarshal(pkg.Data, &conf)
+				if err == nil {
+					handleConf(&conf)
+				} else {
+					log.Println("Error: Failed to unpack MySQL configuration")
+					timod.WriteConfErr()
+				}
+
+			case timod.ProtoModuleReq:
+				onModuleReq(pkg)
+
+			default:
+				log.Printf("Error: Unexpected package type: %d", pkg.Tp)
+			}
+		case err := <-buf.ErrCh:
+			// In case of an error you probably want to quit the module.
+			// ThingsDB will try to restart the module a few times if this
+			// happens.
+			log.Printf("Error: %s", err)
+			quit <- true
+		}
+	}
+}
+
+func main() {
+	// Starts the module
+	timod.StartModule("mysql", handler)
+
+	if db != nil {
+		db.Close()
+	}
+}
---
+++ util.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type _DB interface {
+	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+}
+
+func returnRowsAsMap(rows *sql.Rows) (interface{}, error) {
+	// Get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get columns: %s", err)
+	}
+
+	// Make a slice for the values
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	rowsMap := make([]map[string]interface{}, 0)
+	for rows.Next() {
+		// get RawBytes from data
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to scan rows: %s", err)
+		}
+
+		var value string
+		row := make(map[string]interface{})
+		for i, col := range values {
+			// Here we can check if the value is nil (NULL value)
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			row[columns[i]] = value
+		}
+		rowsMap = append(rowsMap, row)
+	}
+
+	return rowsMap, nil
+}
+
+func execQuery(_db _DB, ctx context.Context, req *reqMySQL) (interface{}, error) {
+	stmt, err := _db.PrepareContext(ctx, req.Query)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to prepare query: %s", err)
+	}
+	defer stmt.Close() // Prepared statements take up server resources and should be closed after use.
+
+	var ret interface{}
+	switch fetch := req.Fetch; fetch {
+	case "LastInsertId":
+		res, err := stmt.ExecContext(ctx, req.Params...)
+		if err != nil {
+			return nil, fmt.Errorf("Query has failed: %s", err)
+		}
+
+		ret, err = res.LastInsertId()
+		if err != nil {
+			return nil, fmt.Errorf("Failed to get last insert ID: %s", err)
+		}
+	case "RowsAffected":
+		res, err := stmt.ExecContext(ctx, req.Params...)
+		if err != nil {
+			return nil, fmt.Errorf("Query has failed: %s", err)
+		}
+
+		ret, err = res.RowsAffected()
+		if err != nil {
+			return nil, fmt.Errorf("Failed to get affected rows: %s", err)
+		}
+	case "Columns":
+		rows, err := stmt.QueryContext(ctx, req.Params...)
+		if err != nil {
+			return nil, fmt.Errorf("Query has failed: %s", err)
+		}
+		defer rows.Close() // ?????????
+
+		ret, err = rows.Columns()
+		if err != nil {
+			return nil, fmt.Errorf("Failed to get columns: %s", err)
+		}
+	case "Rows":
+		rows, err := stmt.QueryContext(ctx, req.Params...)
+		if err != nil {
+			return nil, fmt.Errorf("Query has failed: %s", err)
+		}
+		defer rows.Close() // ?????????
+
+		ret, err = returnRowsAsMap(rows)
+		if err != nil {
+			return nil, err
+		}
+	// case "Row": // Need to no know columns
+		// row := stmt.QueryRowContext(ctx, req.Params...)
+		// err = row.Scan(&ret)
+		// if err != nil {
+		// 	return nil, err
+		// }
+	default:
+		return nil, fmt.Errorf("Fetch parameter unknown; valid options are `Columns`, `Rows`, `LastInsertId` or `RowsAffected`: %s", err)
+	}
+
+	return ret, nil
+}
